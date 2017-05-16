06ec37e6f1fb580b4a92821e1244ab660ae965ac
ascs <ascs@I-PC01.asc-sys.local>
2017-05-16 15:48:49 +0900

    スペアのテストをパラメータによるテストに変更
    リファクタリングのため

--- tst/bowling/BowlingGameTest.java
+++ tst/bowling/BowlingGameTest.java
@@ -3,7 +3,6 @@ package bowling;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
@@ -13,52 +12,38 @@ import org.junit.runner.RunWith;
 public class BowlingGameTest {
 	public 	BowlingGame sut = new BowlingGame();
 
-	@Test
-	public void フレーム1が5_5_フレーム2が1_1_残りガターのテストで13() {
-		sut.shot(5);
-		sut.shot(5);
-		sut.shot(1);
-		sut.shot(1);
-		sameShots(0,16);
-		int actual		= sut.getScore();
-		int expected	= 13;
-		assertThat(actual,is(expected));
-	}
-	@Test
-	public void フレーム1が5_5_フレーム2が5_1_残りガターのテストで21() {
-		sut.shot(5);
-		sut.shot(5);
-		sut.shot(5);
-		sut.shot(1);
-		sameShots(0,16);
-		int actual		= sut.getScore();
-		int expected	= 21;
-		assertThat(actual,is(expected));
-	}
-
-
-
 	@DataPoints
 	public static Fixture[] param = {
-			new Fixture("全投球ガターのテスト",0,0),
-			new Fixture("全投球１のテスト",1,20)
+			new Fixture("全投球ガターのテスト",new int[]{},0,20,0),
+			new Fixture("全投球１のテスト",new int[]{},1,20,20),
+			new Fixture("フレーム1が5_5_フレーム2が1_1_残りガターのテストで13",
+					new int[]{5,5,1,1},0,16,13),
+			new Fixture("フレーム1が5_5_フレーム2が5_1_残りガターのテストで21",
+					new int[]{5,5,5,1},0,16,21),
 	};
 
 	public static class Fixture{
 		String msg;
-		int pins;
+		int pins[];
+		int samePins;
+		int times;
 		int expected;
-		Fixture(String msg,int pins, int expected) {
-			this.msg = msg;
-			this.pins = pins;
-			this.expected = expected;
+		Fixture(String msg,int pins[],int samePins,int times, int expected) {
+			this.msg 		= msg;
+			this.pins		= pins;
+			this.samePins 	= samePins;
+			this.times 		= times;
+			this.expected 	= expected;
 		}
 
 	}
 
 	@Theory
 	public void 全フレーム同じ投球の場合の組み合わせテスト(Fixture param) {
-		sameShots(param.pins,20);
+		for(int pins:param.pins){
+			sut.shot(pins);
+		}
+		sameShots(param.samePins,param.times);
 		int actual		= sut.getScore();
 		int expected	= param.expected;
 		assertThat(param.msg,actual,is(expected));
