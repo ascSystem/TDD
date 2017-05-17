13e70e7840a76cb529dc4fc302bd8f8d2af04cf6
ascs <ascs@I-PC01.asc-sys.local>
2017-05-17 09:42:07 +0900

    テストスイートAllTestを作成
    全てテストするため

---
+++ tst/bowling/AllTests.java
@@ -0,0 +1,11 @@
+package bowling;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BowlingGameTest.class, FrameTest.class })
+public class AllTests {
+
+}
